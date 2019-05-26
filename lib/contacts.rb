eddb31d05875a11aeac6c58485bd15c0b33bcf43
ASHOK MAHARJAN <ashok997@gmail.com>
2019-05-26 02:21:05 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,7 +19,7 @@ def remove_strawberry(contacts)
       hash.each do |attribute,data|
         if attribute == favorite_ice_cream_flavors
           binding.pry
-      end
+      #end
     end
   end
 end
