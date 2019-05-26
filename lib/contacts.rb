143f4d0c902af552476e2bf35666c50f2acc4104
ASHOK MAHARJAN <ashok997@gmail.com>
2019-05-26 02:14:19 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -6,13 +6,13 @@ require 'pry'
       name: "Jon",
       email: "jon_snow@thewall.we", 
       favorite_ice_cream_flavors: ["chocolate", "vanilla"]
-  #   },
-  #   "Freddy Mercury" => {
-  #     name: "Freddy",
-  #     email: "freddy@mercury.com",
-  #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
-  #   }
-  # }
+   },
+   "Freddy Mercury" => {
+     name: "Freddy",
+     email: "freddy@mercury.com",
+     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+   }
+ }
   
 def remove_strawberry(contacts)
   contacts.each do |person, contact_detail_hash|
